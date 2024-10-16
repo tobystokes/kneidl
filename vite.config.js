a661308072429c683bb3d8f7120f45c6912d6661
Toby Stokes <toby@tobystokes.uk>
2024-10-16 17:59:03 +0100

    rm devtools, just use new browser extension

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => ({
@@ -12,9 +11,9 @@ export default defineConfig(({ command, mode }) => ({
   },
   plugins: [
     vue(),
-    vueDevTools(),
     VitePWA({
-      registerType: 'autoUpdate', manifest: {
+      registerType: 'autoUpdate',
+      manifest: {
         name: 'Kneidle',
         short_name: 'Kneidle',
         description: 'My Awesome App description',
